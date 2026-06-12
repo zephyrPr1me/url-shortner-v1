c513c7020c235cec292cb63b9836d0e2953856e3
Hailrake <qwetprime7@gmail.com>
2026-06-12 17:00:20 +0500

    feat: fix bugs added docker infra and structured files added index for models.py URLModel short id removed file based domain check.Added config.py for pydantic settings

--- app/core/database.py
+++ app/core/database.py
@@ -7,9 +7,7 @@ from sqlalchemy.ext.asyncio import (
 from app.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=settings.DB_ECHO)
-async_session = async_sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 async def get_session() -> AsyncSession:
